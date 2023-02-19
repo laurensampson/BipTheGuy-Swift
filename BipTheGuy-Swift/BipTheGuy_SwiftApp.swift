d0d328f18891d049592716358327961aee6e8e8f
laurensampson <sampsonl@bc.edu>
2023-02-19 18:00:21 -0500

    Initial Commit

---
+++ BipTheGuy-Swift/BipTheGuy_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuy_SwiftApp.swift
+//  BipTheGuy-Swift
+//
+//  Created by Lauren Sampson on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuy_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
